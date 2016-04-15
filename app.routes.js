9bda21c83b0835b4d609b9c7de97e5a5ef56bf10
emilym <emilym@paysavvy.com>
2016-04-15 13:55:49 -0700

    Expand routes

--- app.routes.js
+++ app.routes.js
@@ -2,16 +2,26 @@
   'use strict';
 
   angular.module('onboarding')
-    .config(outesConfig);
+    .config(routesConfig);
 
-  routesConfig.$inject = [ '$stateProvider' ];
+  routesConfig.$inject = [ '$stateProvider', '$urlRouterProvider' ];
 
-  function routesConfig($stateProvider) {
+  function routesConfig($stateProvider, $urlRouterProvider) {
 
     $stateProvider
       .state('main', {
         url: '/',
         template: '<rob-main></rob-main>',
+      })
+      .state('main', {
+        url: '/:newbId',
+        template: '<rob-main></rob-main>',
       });
+
+
+
+    $urlRouterProvider.otherwise('/');
+
+
   }
 })();
