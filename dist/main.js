808597e1dd9d81f1ef078efe02b6b968c7beca23
Zachary Alexander Belford <belfordz66@gmail.com>
2016-04-15 14:05:03 -0700

    fixed scss links

--- dist/main.js
+++ dist/main.js
@@ -1,22 +1,7 @@
 (function() {
-(function() {
-angular.module("onboardingTemplates", []).run(["$templateCache", function($templateCache) {$templateCache.put("welcome/welcome.template.html","<div> HEY WHATS UP </div>\n");}]);
-}());
-
 'use strict';
 
 (function () {
   angular.module('onboarding');
-})();
-
-(function () {
-  angular.module('onboarding').directive('welcome', welcomeDirective);
-
-  function welcomeDirective() {
-    return {
-      template: 'views/welcome/welcome.html',
-      scope: {}
-    };
-  }
 })();
 }());
